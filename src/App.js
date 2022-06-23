dd1e1eba00e9cc3ef33be283d4e327fd49130d28
Andrew Owens <drew2447@gmail.com>
2022-06-23 14:45:18 -0600

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./button";
+import { Card, CardContainer } from "./cardContainer";
+import { Title, Text } from "./fonts";
+import { devices } from "./sizes";
+// import "./styles.css";
 
-function App() {
+const Paragraph = ({ children, color }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <p style={{ fontSize: "42px", color: color || "black" }}>{children}</p>
+  );
+};
+
+export default function App() {
+  return (
+    <div>
+      <p>yo</p>
+      <CardContainer>
+        <Card />
+        <Card />
+        <Card />
+      </CardContainer>
+      <p>{devices.tablet}</p>
+      <Title>Title 1 44/56</Title>
+      <Title type="t2">Title 2 44/56</Title>
+      <Title type="t3">Title 3 44/56</Title>
+
+      <Text large dark>
+        Large
+      </Text>
+      <Text>regular</Text>
+      <Text small bold red>
+        small and bold
+      </Text>
+
+      <Text small bold teal>
+        small and bold and dark
+      </Text>
+      <Text small>small</Text>
+      <Text micro>micro</Text>
+
+      <Button>add</Button>
+      <Button red>add</Button>
     </div>
   );
 }
+// Three ways to do styling in react
 
-export default App;
+// 1. OLD school CSS
+// 2. inline styling (THIS IS BAD!,  but in react it is ok)
+// how do I do animations? rensponsive stuff, :active?
+
+// 3. styled components...
+
+// When it comes it styling, if you want to make something look good
+// 1. consistency
+// 2. we don't want duplicate hardcoded vairables
