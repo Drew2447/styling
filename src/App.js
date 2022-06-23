7a29cdb1994c22a2f64f23643431aa8bc1ac01bd
Andrew Owens <drew2447@gmail.com>
2022-06-23 15:19:13 -0600

    update everthingis working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import Button from "./button";
 import { Card, CardContainer } from "./cardContainer";
 import { Title, Text } from "./fonts";
-import { devices } from "./sizes";
-// import "./styles.css";
 
 const Paragraph = ({ children, color }) => {
   return (
@@ -13,44 +11,35 @@ const Paragraph = ({ children, color }) => {
 export default function App() {
   return (
     <div>
-      <p>yo</p>
+      <p>Hello</p>
       <CardContainer>
         <Card />
         <Card />
         <Card />
+        <Card />
+        <Card />
       </CardContainer>
-      <p>{devices.tablet}</p>
-      <Title>Title 1 44/56</Title>
-      <Title type="t2">Title 2 44/56</Title>
-      <Title type="t3">Title 3 44/56</Title>
+      <p>Andrew's Been Here</p>
+      <Title>How</Title>
+      <Title type="t2">Is</Title>
+      <Title type="t3">My</Title>
 
       <Text large dark>
-        Large
+        App
       </Text>
-      <Text>regular</Text>
+      <Text>Looking</Text>
       <Text small bold red>
-        small and bold
+        For
       </Text>
 
       <Text small bold teal>
-        small and bold and dark
+        Not
       </Text>
-      <Text small>small</Text>
-      <Text micro>micro</Text>
+      <Text small>Knowing</Text>
+      <Text micro>Anything</Text>
 
-      <Button>add</Button>
-      <Button red>add</Button>
+      <Button>Up</Button>
+      <Button>Down</Button>
     </div>
   );
 }
-// Three ways to do styling in react
-
-// 1. OLD school CSS
-// 2. inline styling (THIS IS BAD!,  but in react it is ok)
-// how do I do animations? rensponsive stuff, :active?
-
-// 3. styled components...
-
-// When it comes it styling, if you want to make something look good
-// 1. consistency
-// 2. we don't want duplicate hardcoded vairables
